5e4cd43900c0d52a4d1ba5f20323d6ac39429560
Christopher Romero <romero.chrisprof@gmail.com>
2024-08-05 01:35:12 -0500

    Finished post request

--- Develop/server.js
+++ Develop/server.js
@@ -8,6 +8,9 @@ const fs = require("fs");
 // ? not really sure what this does
 const path = require('path');
 
+// ?
+const uuid = require('./helpers/uuid');
+
 //Where we are going to be importing, focus on Render.com after modularizing
 PORT = 3001;
 
@@ -23,20 +26,20 @@ app.use(express.static('public'));
 
 // Create HTML Routes
 
-// GET /notes should return the notes.html file
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
-
 // GET * should return the index.html file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+// GET /notes should return the notes.html file
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
 // Create API Routes
 
-// GET /api/routes should read the db.json file and return all saved notes as JSON
-app.get('/api/routes', (req, res) => {
+// GET /api/notes should read the db.json file and return all saved notes as JSON
+app.get('/api/notes', (req, res) => {
     console.info(`${req.method} request was sent to return all saved notes`);
     //read json file
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
@@ -52,6 +55,51 @@ app.get('/api/routes', (req, res) => {
 // POST /api/notes should recieve a new note to save on the request body, add it to the db.json file, 
 // and then return the new note to the client. You'll need to find a way to give each note a unique id 
 // when it's saved (look into npm packages that could do this for you).
+
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a new note`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        id: uuid()
+      };
+
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedNotes = JSON.parse(data);
+
+            // Add new note
+            parsedNotes.push(newNote);
+
+            //write updated notes back into file
+            fs.writeFile('./db/db.json', JSON.stringify(parsedNotes), (writeErr) => {
+                writeErr ? console.error(writeErr) : console.info('Successfully updated Notes!');
+            });
+        }
+      });
+
+      const response = {
+        status: 'sucess',
+        body: newNote
+      };
+
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting note');
+    }
+      
+    });
 
 // BONUS: DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete.
 // In order to delete a note, you'll need to read all notes from the db.json file, remove the note with 
